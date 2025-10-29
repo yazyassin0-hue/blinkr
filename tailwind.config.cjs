368fb3f6d61bbebed58364e21ec426578feff2f9
yazyassin0-hue <yazyassin0@gmail.com>
2025-10-29 03:39:24 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f5fbfb',
+          100: '#e6f6f7',
+          500: '#0ea5a0',
+          600: '#0b8f8a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
