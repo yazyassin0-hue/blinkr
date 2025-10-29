061a9e629ec626043c59fe749d16670f152f9471
yazyassin0-hue <yazyassin0@gmail.com>
2025-10-29 05:07:59 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import Chat from './pages/Chat';
+import './styles.css';
+import './i18n';
+
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/auth' element={<Auth/>} />
+        <Route path='/chat' element={<Chat/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App/>);
